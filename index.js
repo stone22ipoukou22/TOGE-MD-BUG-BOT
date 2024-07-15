61b0a389382a8882e27a1dfb8325808576664b7c
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-07-15 22:28:00 +0100

    Update

--- index.js
+++ index.js
@@ -51,3 +51,10 @@ function start() {
       })
 }
 start()
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
